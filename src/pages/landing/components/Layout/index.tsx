c1405617261344585c1cc75424efed0e4aa87905
Haerul <haerul.muttaqin@gmail.com>
2024-12-20 15:59:38 +0700

    fix meta tag

--- src/pages/landing/components/Layout/index.tsx
+++ src/pages/landing/components/Layout/index.tsx
@@ -22,26 +22,26 @@ const LandingPageLayout: FC<LandingPageLayoutProps> = (
                 <meta name="author" content="Haerul Muttaqin"/>
                 <meta name="robots" content="index, follow"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="title" content={`${process.env.NEXT_PUBLIC_APP_NAME}`} key="title"/>
-                <meta name="url" content={`${process.env.NEXT_PUBLIC_PAGE_URL}`}/>
+                <meta name="title" content="Mock N' Roll"/>
+                <meta name="url" content="https://mocknroll.me"/>
                 <meta name="description"
                       content="Streamline your development workflow! Our platform offers instant mock API creation, real-time testing, and seamless integration"/>
                 <meta name="keywords" content="Mock API, API Generator FREE, Free Mock API"/>
                 <meta name="image" content="https://mocknroll.me/mocknroll.webp"/>
-                <meta property="og:title" content={`${process.env.NEXT_PUBLIC_APP_NAME}`} key="title"/>
-                <meta property="og:url" content={process.env.NEXT_PUBLIC_PAGE_URL}/>
+                <meta property="og:title" content="Mock N' Roll"/>
+                <meta property="og:url" content="https://mocknroll.me"/>
                 <meta property="og:description"
                       content="Streamline your development workflow! Our platform offers instant mock API creation, real-time testing, and seamless integration"/>
                 <meta property="og:keywords" content="Mock API, API Generator FREE, Free Mock API"/>
                 <meta property="og:image" content="https://mocknroll.me/mocknroll.webp"/>
-                <meta itemProp="title" content={`${process.env.NEXT_PUBLIC_APP_NAME}`} key="title"/>
-                <meta itemProp="url" content={`${process.env.NEXT_PUBLIC_PAGE_URL}`}/>
+                <meta itemProp="title" content="Mock N' Roll"/>
+                <meta itemProp="url" content="https://mocknroll.me"/>
                 <meta itemProp="description"
                       content="Streamline your development workflow! Our platform offers instant mock API creation, real-time testing, and seamless integration"/>
                 <meta itemProp="keywords" content="Mock API, API Generator FREE, Free Mock API"/>
                 <meta itemProp="image" content="https://mocknroll.me/mocknroll.webp"/>
-                <meta property="twitter:title" content={`${process.env.NEXT_PUBLIC_APP_NAME}`} key="title"/>
-                <meta property="twitter:url" content={`${process.env.NEXT_PUBLIC_PAGE_URL}`}/>
+                <meta property="twitter:title" content="Mock N' Roll"/>
+                <meta property="twitter:url" content="https://mocknroll.me"/>
                 <meta property="twitter:description"
                       content="Streamline your development workflow! Our platform offers instant mock API creation, real-time testing, and seamless integration"/>
                 <meta property="twitter:keywords" content="Mock API, API Generator FREE, Free Mock API"/>
